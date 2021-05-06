827b1648c1c52f4599a76b8733d0c3d3e4918ffa
chrismad26 <cmadison@chicagobooth.edu>
2021-05-06 05:53:18 +0000

    tabs-built

--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,8 @@ Rails.application.routes.draw do
 
   get ":username/liked" => "photos#liked", as: :liked_photos
   get ":username/feed" => "photos#feed", as: :feed_photos
-  get ":username/followers" => "photos#followers", as: :followers_photos
-  get ":username/following" => "photos#following", as: :following_photos
+  get ":username/followers" => "users#followers", as: :followers_users
+  get ":username/following" => "users#following", as: :following_users
   get ":username" => "users#show", as: :user
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
